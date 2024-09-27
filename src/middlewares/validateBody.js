46eea2459d2b3b9554dcbc1877cebddc665c6652
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2024-09-26 21:36:28 -0300

    Alteração no middleware de validação de corpo

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,9 +1,20 @@
+import schemaErros from "../schemas/errors-schema.js"
+
 const validateBody = (schema) => async (req, res, next) => {
     try {
         await schema.validateAsync(req.body)
         next()
     } catch (error) {
-        res.status(400).json({ Mensagem: `${error.message}`})
+        const nomeDoErroIngles = error.details[0].type
+        const nomeDoErroPortugues = schemaErros.mensagemErrorTraduzida
+        const nomeDoCampo = error.details[0].path[0]
+
+        schemaErros.tipoDosErros.forEach((erro, i) => {
+            
+            if (erro == nomeDoErroIngles) return res.status(400).json({ Mensagem: nomeDoErroPortugues[i].replace("$", nomeDoCampo) })
+        })
+
+        // return res.status(500).json({ Mensagem: ` Erro interno no servidor: ${error.message}` })
     }
 }
 
