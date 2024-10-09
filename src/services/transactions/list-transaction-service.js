202b453e1fcbea9e371b8570da40261828c8d52c
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2024-10-09 19:05:45 -0300

    Alteração na rota de listar transação

--- src/services/transactions/list-transaction-service.js
+++ src/services/transactions/list-transaction-service.js
@@ -4,6 +4,7 @@ const prisma = new PrismaClient();
 import { appError } from "../../errors/app-errors.js"
 
 const serviceLista = async (userId, queryId, page, limit) => {
+    let listarTransacao
     if (!queryId) {
         appError("Parâmetro necessário ausente.", 400)
     }
@@ -11,24 +12,49 @@ const serviceLista = async (userId, queryId, page, limit) => {
     if (userId != queryId) {
         appError("Você não tem permissão para listar esse usuário.", 403)
     } else {
-        const skip = (page - 1) * limit;
-        const listarTransacao = await prisma.transacao.findMany({
-            where: {
-                usuario_id: Number(queryId)
-            },
-            select: {
-                id: true,
-                valor: true,
-                tipo: true,
-                categoria: true,
-                usuario_id: true
-            },
-            orderBy: {
-                id: "asc"
-            },
-            skip: skip,
-            take: Number(limit),
-        })
+        if (!page && !limit) {
+            listarTransacao = await prisma.transacao.findMany({
+                where: {
+                    usuario_id: Number(queryId)
+                },
+                select: {
+                    id: true,
+                    valor: true,
+                    tipo: true,
+                    categoria: true,
+                    usuario_id: true
+                },
+                orderBy: {
+                    id: "asc"
+                }
+            })
+
+            return {
+                listarTransacao,
+            }
+            
+        } else {
+            const skip = (page - 1) * limit;
+            listarTransacao = await prisma.transacao.findMany({
+                where: {
+                    usuario_id: Number(queryId)
+                },
+                select: {
+                    id: true,
+                    valor: true,
+                    tipo: true,
+                    categoria: true,
+                    usuario_id: true
+                },
+                orderBy: {
+                    id: "asc"
+                },
+                skip: skip,
+                take: Number(limit),
+            })
+            
+        }
+        
         const totalTransacoes = await prisma.transacao.count({
             where: {
                 usuario_id: Number(queryId),
