3410cc7510c4a82a417d8818b8976ed4f6ba5ca1
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2024-10-12 04:34:12 -0300

    Email esqueceu sua senha

--- src/services/user/mail-user-service.js
+++ src/services/user/mail-user-service.js
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer"
 import crypto from "crypto"
+import jwt from "jsonwebtoken";
 
 function senhaAleatoria(length = 15) {
     return crypto.randomBytes(length).toString('hex').slice(0, length);
@@ -7,6 +8,10 @@ function senhaAleatoria(length = 15) {
 
 
 const serviceRedefinirSenha = (destinatario) => {
+    const token = jwt.sign({ destinatario }, process.env.SECRET_KEY, { expiresIn: "1h" })
+
+    const link = `https://controle-financeiro-frontend-mocha.vercel.app?token=${token}`
+
     const transporter = nodemailer.createTransport({
         host: process.env.MAIL_HOST,
         port: process.env.MAIL_PORT,
@@ -18,10 +23,10 @@ const serviceRedefinirSenha = (destinatario) => {
     })
 
     transporter.sendMail({
-        from: process.env.MAIL_FROM,
+        from: "Luccas Marinho <luccas88oliveira@gmail.com>",
         to: destinatario,
         subject: "Redefinição de senha",
-        text: `Sua senha é ${senhaAleatoria()}`
+        text: `Você solicitou a alteração de senha. Clique neste link e altere-a: ${link}. Caso não tenha solicitado, ignore esse E-mail.`
     })
 }
 
