d6a65e8c324e493e82396dc08ab4a3a726ced965
Luccas Oliveira Marinho da Silva <luccas88oliveira@gmail.com>
2024-10-17 16:55:56 -0300

    Async/await email

--- src/controllers/user/mail-user-controller.js
+++ src/controllers/user/mail-user-controller.js
@@ -1,9 +1,14 @@
 import serviceEnviarEmail from "../../services/user/mail-user-service.js"
 
-const enviarEmail = (req, res) => {
-    const { email } = req.body
-    serviceEnviarEmail(email)
-    return res.status(200).json({ Mensagem: "Email enviado com sucesso." })
+const enviarEmail = async (req, res) => {
+    try {
+        const { email } = req.body
+        await serviceEnviarEmail(email)
+        return res.status(200).json({ Mensagem: "Email enviado com sucesso." })
+    } catch (error) {
+        res.status(500).json({ Mensagem: `Erro interno do servidor: ${error.message}` })
+    }
+
 }
 
 export default enviarEmail
